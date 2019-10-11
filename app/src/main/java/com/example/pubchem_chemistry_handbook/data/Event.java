ac61e8f22939db91a633de1fa04397ffd9eaae7b
xavier wells <xavier.wells@mavs.uta.edu>
2019-10-10 21:33:41 -0500

    Functional News

--- app/src/main/java/com/example/pubchem_chemistry_handbook/data/Event.java
+++ app/src/main/java/com/example/pubchem_chemistry_handbook/data/Event.java
@@ -1,17 +1,19 @@
 package com.example.pubchem_chemistry_handbook.data;
 
-import java.net.URL;
 
 public class Event {
     String title;
     String description;
     String link;
+    String date;
 
-    public Event(String title, String description, String link) {
+    public Event(String title, String description, String link, String date) {
         this.title = title;
         this.description = description;
         this.link=link;
     }
+    public Event(){}
+
     public String getTitle() {
         return title;
     }
@@ -20,6 +22,14 @@ public class Event {
 
     public String getLink() {
         return link;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 
     public void setTitle(String title) {
--- app/src/main/java/com/example/pubchem_chemistry_handbook/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/pubchem_chemistry_handbook/ui/news/NewsFragment.java
@@ -1,9 +1,13 @@
 package com.example.pubchem_chemistry_handbook.ui.news;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -18,38 +22,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.pubchem_chemistry_handbook.R;
 import com.example.pubchem_chemistry_handbook.data.Event;
 
-import java.util.Arrays;
+import org.xmlpull.v1.XmlPullParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NewsFragment extends Fragment {
 
     private NewsViewModel newsViewModel;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
+    static List<Event> events = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         newsViewModel =
                 ViewModelProviders.of(this).get(NewsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_news, container, false);
-        final RecyclerView RSS_Feed = (RecyclerView) root.findViewById(R.id.RSS_recycler_view);
+
         final TextView textDate = root.findViewById(R.id.news_date);
         final TextView textDay = root.findViewById(R.id.news_day);
-
-        RSS_Feed.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-
-        List<Event> events = Arrays.asList( new Event("Title", "Desc", "Link"),
-                new Event("Title1", "Desc1", "Link1"),
-                new Event("Title2", "Desc2", "Link2"),
-                new Event("Title3", "Desc3", "Link3"),
-                new Event("Title4", "Desc4", "Link4"),
-                new Event("Title5", "Desc5", "Link5"));
-        RSS_Adapter mAdapter = new RSS_Adapter(events);
-        RSS_Feed.setAdapter(mAdapter);
-        RSS_Feed.setItemAnimator(new DefaultItemAnimator());
-        
-        
-        
 
         newsViewModel.getDate().observe(this, new Observer<String>() {
             @Override
@@ -65,9 +58,102 @@ public class NewsFragment extends Fragment {
             }
         });
 
+        new getXML(root).execute("https://rss.sciencedaily.com/matter_energy/chemistry.xml");
 
         return root;
+
+
     }
 
+    //////
+//////
+//////
+    static final String PUB_DATE = "pubDate";
+    static final String DESCRIPTION = "description";
+    static final String CHANNEL = "channel";
+    static final String LINK = "link";
+    static final String TITLE = "title";
+    static final String ITEM = "item";
 
+    static private class getXML extends AsyncTask<String, String, List<Event>> {
+        View view;
+        public getXML(View view){
+            this.view=view;
+        }
+        @Override
+        protected List<Event> doInBackground(String... strings) {
+            String rssFeed = strings[0];
+            List<Event> list = new ArrayList<Event>();
+            XmlPullParser parser = Xml.newPullParser();
+            InputStream stream = null;
+            try {
+                // auto-detect the encoding from the stream
+                stream = new URL(rssFeed).openConnection().getInputStream();
+                parser.setInput(stream, null);
+                int eventType = parser.getEventType();
+                boolean done = false;
+                Event item = null;
+                while (eventType != XmlPullParser.END_DOCUMENT && !done) {
+                    String name = null;
+                    switch (eventType) {
+                        case XmlPullParser.START_DOCUMENT:
+                            break;
+                        case XmlPullParser.START_TAG:
+                            name = parser.getName();
+                            if (name.equalsIgnoreCase(ITEM)) {
+                                Log.i("new item", "Create new item");
+                                item = new Event();
+                            } else if (item != null) {
+                                if (name.equalsIgnoreCase(LINK)) {
+                                    Log.i("Attribute", "setLink");
+                                    item.setLink(parser.nextText());
+                                } else if (name.equalsIgnoreCase(DESCRIPTION)) {
+                                    Log.i("Attribute", "description");
+                                    item.setDescription(parser.nextText().trim());
+                                } else if (name.equalsIgnoreCase(PUB_DATE)) {
+                                    Log.i("Attribute", "date");
+                                    item.setDate(parser.nextText());
+                                } else if (name.equalsIgnoreCase(TITLE)) {
+                                    Log.i("Attribute", "title");
+                                    item.setTitle(parser.nextText().trim());
+                                }
+                            }
+                            break;
+                        case XmlPullParser.END_TAG:
+                            name = parser.getName();
+                            Log.i("End tag", name);
+                            if (name.equalsIgnoreCase(ITEM) && item != null) {
+                                Log.i("Added", item.toString());
+                                list.add(item);
+                            } else if (name.equalsIgnoreCase(CHANNEL)) {
+                                done = true;
+                            }
+                            break;
+                    }
+                    eventType = parser.next();
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (stream != null) {
+                    try {
+                        stream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return list;
+        }
+
+        @Override
+        protected void onPostExecute(List<Event> res) {
+            events=res;
+            RecyclerView RSS_Feed = view.findViewById(R.id.RSS_recycler_view);
+            RSS_Feed.setLayoutManager(new LinearLayoutManager(view.getContext()));
+            RSS_Adapter mAdapter = new RSS_Adapter(events);
+            RSS_Feed.setAdapter(mAdapter);
+            RSS_Feed.setItemAnimator(new DefaultItemAnimator());
+        }
+    }
 }
