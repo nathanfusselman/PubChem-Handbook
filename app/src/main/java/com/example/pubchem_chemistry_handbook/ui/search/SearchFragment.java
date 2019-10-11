640186c86f17c6b4b6af403e5feb6e77ddd825ad
Nathan Fusselman <nathanfusselman@icloud.com>
2019-10-11 13:38:51 -0500

    Safety Added
    Removed Formula pard due to large json differences and bugs.

--- app/src/main/java/com/example/pubchem_chemistry_handbook/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/pubchem_chemistry_handbook/ui/search/SearchFragment.java
@@ -85,11 +85,43 @@ public class SearchFragment extends Fragment {
         final TextView compoundView_formula = view.findViewById(R.id.compoundView_formula);
         final ImageView compoundView_2dImage = view.findViewById(R.id.compoundView_2dImage);
         final ImageView compoundView_3dImage = view.findViewById(R.id.compoundView_3dImage);
+        final ImageView compoundView_crystal = view.findViewById(R.id.compoundView_crystal);
         final Button compoundView_backButton = view.findViewById(R.id.closeButton);
         CheckBox search_type_startsWith = view.findViewById(R.id.search_type_startsWith);
         final LinearLayout SafetyItems_Images = view.findViewById(R.id.SafetyItems_Images);
         final LinearLayout SafetyItems_Text = view.findViewById(R.id.SafetyItems_Text);
         final SearchView searchView = view.findViewById(R.id.searchView);
+        final ImageView[] HazardImages = new ImageView[9];
+        final TextView[] HazardTexts = new TextView[9];
+        HazardImages[0] = view.findViewById(R.id.SafetyItems_Images_GHS01);
+        HazardTexts[0] = view.findViewById(R.id.SafetyItems_Text_GHS01);
+        HazardImages[1] = view.findViewById(R.id.SafetyItems_Images_GHS02);
+        HazardTexts[1] = view.findViewById(R.id.SafetyItems_Text_GHS02);
+        HazardImages[2] = view.findViewById(R.id.SafetyItems_Images_GHS03);
+        HazardTexts[2] = view.findViewById(R.id.SafetyItems_Text_GHS03);
+        HazardImages[3] = view.findViewById(R.id.SafetyItems_Images_GHS04);
+        HazardTexts[3] = view.findViewById(R.id.SafetyItems_Text_GHS04);
+        HazardImages[4] = view.findViewById(R.id.SafetyItems_Images_GHS05);
+        HazardTexts[4] = view.findViewById(R.id.SafetyItems_Text_GHS05);
+        HazardImages[5] = view.findViewById(R.id.SafetyItems_Images_GHS06);
+        HazardTexts[5] = view.findViewById(R.id.SafetyItems_Text_GHS06);
+        HazardImages[6] = view.findViewById(R.id.SafetyItems_Images_GHS07);
+        HazardTexts[6] = view.findViewById(R.id.SafetyItems_Text_GHS07);
+        HazardImages[7] = view.findViewById(R.id.SafetyItems_Images_GHS08);
+        HazardTexts[7] = view.findViewById(R.id.SafetyItems_Text_GHS08);
+        HazardImages[8] = view.findViewById(R.id.SafetyItems_Images_GHS09);
+        HazardTexts[8] = view.findViewById(R.id.SafetyItems_Text_GHS09);
+
+        final LinearLayout StructureImageLayout = view.findViewById(R.id.compoundView_images);
+        final LinearLayout StructureTextLayout = view.findViewById(R.id.compoundView_images_names);
+        final ImageView[] StructureImages = new ImageView[3];
+        final TextView[] StructureTexts = new TextView[3];
+        StructureImages[0] = view.findViewById(R.id.compoundView_2dImage);
+        StructureTexts[0] = view.findViewById(R.id.compoundView_images_names_2d);
+        StructureImages[1] = view.findViewById(R.id.compoundView_3dImage);
+        StructureTexts[1] = view.findViewById(R.id.compoundView_images_names_3d);
+        StructureImages[2] = view.findViewById(R.id.compoundView_crystal);
+        StructureTexts[2] = view.findViewById(R.id.compoundView_images_names_crystal);
         resutlsNumb.setText("Results: " + global.getResults());
         compoundView_backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -187,54 +219,131 @@ public class SearchFragment extends Fragment {
                                     list_compound.get(position).setEID((int) RecordNumber);
                                     //Toast.makeText(getActivity(), "RecordNumber: " + RecordNumber, Toast.LENGTH_LONG).show();
                                     JSONArray section = (JSONArray) record.get("Section");
-                                    JSONObject section_2 = (JSONObject) section.get(2);
-                                    JSONArray sub_section_2 = (JSONArray) section_2.get("Section");
-                                    JSONObject sub_section_2_2 = (JSONObject) sub_section_2.get(2);
-                                    JSONArray Information = (JSONArray) sub_section_2_2.get("Information");
-                                    JSONObject sub_Information = (JSONObject) Information.get(0);
-                                    JSONObject Value = (JSONObject) sub_Information.get("Value");
-                                    JSONArray StringWithMarkup = (JSONArray) Value.get("StringWithMarkup");
-                                    JSONObject sub_StringWithMarkup = (JSONObject) StringWithMarkup.get(0);
-                                    String RecordFormula = (String) sub_StringWithMarkup.get("String");
+                                    JSONObject section_0 = (JSONObject) section.get(0);
+                                    JSONArray structure_section = (JSONArray) section_0.get("Section");
+                                    /*
+                                    String RecordFormula = "";
+                                    try {
+                                        structure_section = (JSONArray) section_0.get("Section");
+                                        JSONObject section_2 = (JSONObject) section.get(2);
+                                        JSONArray sub_section_2 = (JSONArray) section_2.get("Section");
+                                        JSONObject sub_section_2_2 = (JSONObject) sub_section_2.get(2);
+                                        JSONArray Information = (JSONArray) sub_section_2_2.get("Information");
+                                        JSONObject sub_Information = (JSONObject) Information.get(0);
+                                        JSONObject Value = (JSONObject) sub_Information.get("Value");
+                                        JSONArray StringWithMarkup = (JSONArray) Value.get("StringWithMarkup");
+                                        JSONObject sub_StringWithMarkup = (JSONObject) StringWithMarkup.get(0);
+                                        RecordFormula = (String) sub_StringWithMarkup.get("String");
+                                    } catch (ArrayIndexOutOfBoundsException a) {
+                                        section = (JSONArray) record.get("Section");
+                                        JSONObject section_0 = (JSONObject) section.get(0);
+                                        structure_section = (JSONArray) section_0.get("Section");
+                                        JSONObject section_1 = (JSONObject) section.get(1);
+                                        JSONArray sub_section_1 = (JSONArray) section_1.get("Section");
+                                        JSONObject sub_section_1_1 = (JSONObject) sub_section_1.get(1);
+                                        JSONArray Information = (JSONArray) sub_section_1_1.get("Information");
+                                        JSONObject sub_Information = (JSONObject) Information.get(0);
+                                        JSONObject Value = (JSONObject) sub_Information.get("Value");
+                                        JSONArray StringWithMarkup = (JSONArray) Value.get("StringWithMarkup");
+                                        JSONObject sub_StringWithMarkup = (JSONObject) StringWithMarkup.get(0);
+                                        RecordFormula = (String) sub_StringWithMarkup.get("String");
+                                    } catch (IndexOutOfBoundsException i) {
+                                        section = (JSONArray) record.get("Section");
+                                        JSONObject section_0 = (JSONObject) section.get(0);
+                                        structure_section = (JSONArray) section_0.get("Section");
+                                        JSONObject section_1 = (JSONObject) section.get(1);
+                                        JSONArray sub_section_1 = (JSONArray) section_1.get("Section");
+                                        JSONObject sub_section_1_1 = (JSONObject) sub_section_1.get(1);
+                                        JSONArray Information = (JSONArray) sub_section_1_1.get("Information");
+                                        JSONObject sub_Information = (JSONObject) Information.get(0);
+                                        JSONObject Value = (JSONObject) sub_Information.get("Value");
+                                        JSONArray StringWithMarkup = (JSONArray) Value.get("StringWithMarkup");
+                                        JSONObject sub_StringWithMarkup = (JSONObject) StringWithMarkup.get(0);
+                                        RecordFormula = (String) sub_StringWithMarkup.get("String");
+                                    }
                                     list_compound.get(position).setFormula(RecordFormula);
                                     //Toast.makeText(getActivity(), "RecordFormula: " + RecordFormula, Toast.LENGTH_LONG).show();
-                                    JSONObject section_1 = (JSONObject) section.get(1);
-                                    JSONArray Information_1 = (JSONArray) section_1.get("Information");
-                                    JSONObject sub_Information_1 = (JSONObject) Information_1.get(0);
-                                    JSONObject Value_1 = (JSONObject) sub_Information_1.get("Value");
-                                    JSONArray StringWithMarkup_1 = (JSONArray) Value_1.get("StringWithMarkup");
-                                    JSONObject sub_StringWithMarkup_1 = (JSONObject) StringWithMarkup_1.get(0);
-                                    JSONArray Markup = (JSONArray) sub_StringWithMarkup_1.get("Markup");
-                                    boolean[] safety = new boolean[9];
-                                    for (int i = 0; i < 9; i++) {
-                                        safety[i] = false;
-                                    }
-                                    for (int i = 0; i < Markup.size(); i++) {
-                                        JSONObject sub_Markup = (JSONObject) Markup.get(i);
-                                        String url = (String) sub_Markup.get("URL");
-                                        String name = (String) sub_Markup.get("Extra");
-                                        list_compound.get(position).addSafetyItem(name, url);
-                                        //System.out.println("Added Safety: " + name + ", " + url);
-                                    }
-                                    for (SafetyItem item : list_compound.get(position).getSafetyItems()) {
-                                        int n = Integer.parseInt(String.valueOf(item.getUrl().charAt(48)));
-                                        safety[n] = true;
-                                    }
-                                    for (int i = 8; i >= 0; i--) {
-                                        if (!safety[i]) {
-                                            //System.out.println("Number of Items: " + SafetyItems_Images.getChildCount());
-                                            //System.out.println("Removing Item: " + i);
-                                            if (i <= SafetyItems_Images.getChildCount()) {
-                                                SafetyItems_Images.removeViewAt(i);
-                                                SafetyItems_Text.removeViewAt(i);
+
+                                     */
+                                    StructureImageLayout.removeAllViews();
+                                    StructureTextLayout.removeAllViews();
+                                    try {
+                                        for (int i = 0; i < 3; i++) {
+                                            if (structure_section.size() > i) {
+                                                JSONObject struct_1 = (JSONObject) structure_section.get(i);
+                                                String struct_name = (String) struct_1.get("TOCHeading");
+                                                if (struct_name.equals("2D Structure")) {
+                                                    StructureImageLayout.addView(StructureImages[0]);
+                                                    StructureTextLayout.addView(StructureTexts[0]);
+                                                    AsyncTaskLoadImage image_Loader = new AsyncTaskLoadImage(compoundView_2dImage);
+                                                    image_Loader.execute("https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=" + list_compound.get(position).getEID() + "&t=s");
+                                                }
+                                                if (struct_name.equals("3D Conformer")) {
+                                                    StructureImageLayout.addView(StructureImages[1]);
+                                                    StructureTextLayout.addView(StructureTexts[1]);
+                                                    AsyncTaskLoadImage image_Loader = new AsyncTaskLoadImage(compoundView_3dImage);
+                                                    image_Loader.execute("https://pubchem.ncbi.nlm.nih.gov/image/img3d.cgi?cid=" + list_compound.get(position).getEID() + "&t=s");
+                                                }
+                                                if (struct_name.equals("Crystal Structures")) {
+                                                    JSONArray temp = (JSONArray) struct_1.get("Section");
+                                                    JSONObject temp_1 = (JSONObject) temp.get(1);
+                                                    JSONArray temp_2 = (JSONArray) temp_1.get("Information");
+                                                    JSONObject temp_3 = (JSONObject) temp_2.get(1);
+                                                    JSONObject value = (JSONObject) temp_3.get("Value");
+                                                    JSONArray contents = (JSONArray) value.get("ExternalDataURL");
+                                                    String ExternalURLData = (String) contents.get(0);
+                                                    StructureImageLayout.addView(StructureImages[2]);
+                                                    StructureTextLayout.addView(StructureTexts[2]);
+                                                    AsyncTaskLoadImage image_Loader = new AsyncTaskLoadImage(compoundView_crystal);
+                                                    image_Loader.execute(ExternalURLData);
+                                                }
                                             }
                                         }
+                                    } catch (NullPointerException e) {
+                                    } catch (IndexOutOfBoundsException i) {
+                                    }
+                                    SafetyItems_Images.removeAllViews();
+                                    SafetyItems_Text.removeAllViews();
+                                    try {
+                                        JSONObject section_1 = (JSONObject) section.get(1);
+                                        JSONArray Information_1 = (JSONArray) section_1.get("Information");
+                                        JSONObject sub_Information_1 = (JSONObject) Information_1.get(0);
+                                        JSONObject Value_1 = (JSONObject) sub_Information_1.get("Value");
+                                        JSONArray StringWithMarkup_1 = (JSONArray) Value_1.get("StringWithMarkup");
+                                        JSONObject sub_StringWithMarkup_1 = (JSONObject) StringWithMarkup_1.get(0);
+                                        JSONArray Markup = (JSONArray) sub_StringWithMarkup_1.get("Markup");
+                                        boolean[] safety = new boolean[9];
+                                        for (int i = 0; i < 9; i++) {
+                                            safety[i] = false;
+                                        }
+                                        for (int i = 0; i < Markup.size(); i++) {
+                                            JSONObject sub_Markup = (JSONObject) Markup.get(i);
+                                            String url = (String) sub_Markup.get("URL");
+                                            String name = (String) sub_Markup.get("Extra");
+                                            list_compound.get(position).addSafetyItem(name, url);
+                                            //System.out.println("Added Safety: " + name + ", " + url);
+                                        }
+                                        for (SafetyItem item : list_compound.get(position).getSafetyItems()) {
+                                            int n = Integer.parseInt(String.valueOf(item.getUrl().charAt(48)));
+                                            safety[n-1] = true;
+                                        }
+                                        for (int i = 0; i < 9; i++) {
+                                            if (safety[i]) {
+                                                SafetyItems_Images.addView(HazardImages[i]);
+                                                SafetyItems_Text.addView(HazardTexts[i]);
+                                            }
+                                        }
+                                    } catch (NullPointerException e) {
                                     }
                                 } catch (FileNotFoundException e) {
                                     e.printStackTrace();
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 } catch (ParseException e) {
+                                    e.printStackTrace();
+                                } catch (NullPointerException e) {
+                                    e.printStackTrace();
+                                } catch (IndexOutOfBoundsException e) {
                                     e.printStackTrace();
                                 }
                             }
@@ -247,10 +356,6 @@ public class SearchFragment extends Fragment {
                         });
                 compoundView_name.setText(" " + list_compound.get(position).getName());
                 compoundView_formula.setText("  " + list_compound.get(position).getFormula());
-                AsyncTaskLoadImage image_Loader_2d = new AsyncTaskLoadImage(compoundView_2dImage);
-                image_Loader_2d.execute("https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=" + list_compound.get(position).getEID() + "&t=s");
-                AsyncTaskLoadImage image_Loader_3d = new AsyncTaskLoadImage(compoundView_3dImage);
-                image_Loader_3d.execute("https://pubchem.ncbi.nlm.nih.gov/image/img3d.cgi?&cid=" + list_compound.get(position).getEID() + "&t=s");
                 compoundView.setVisibility(View.VISIBLE);
             }
         });
