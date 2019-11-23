770a0c77d80a585dc2c3cb8827eba546146f6517
xavier wells <xavier.wells@mavs.uta.edu>
2019-11-22 18:30:22 -0600

    saves xml stream for offline use

--- app/src/main/java/com/example/pubchem_chemistry_handbook/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/pubchem_chemistry_handbook/ui/news/NewsFragment.java
@@ -4,8 +4,10 @@ package com.example.pubchem_chemistry_handbook.ui.news;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -29,18 +32,40 @@ import com.example.pubchem_chemistry_handbook.data.Event;
 
 import org.xmlpull.v1.XmlPullParser;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+    private String fullPath=null;
     private TextView loadingText;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        final File directory = getContext().getExternalFilesDir(null);
+        final String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        assert directory != null;
+        fullPath= directory.toString() + "/" + date + ".xml";
+        File toCheck = new File(fullPath);
+
+        if(!toCheck.exists())
+        {
+            deleteFiles(directory.toString()); //cleans up old RSSFeeds
+        }
+
         if(getActivity()!=null){
             InputMethodManager imm = (InputMethodManager) getActivity()
                     .getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -75,7 +100,7 @@ public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         WeakReference<View> weakView;
         weakView = new WeakReference<>(root);
         WeakReference<TextView> weakTextView = new WeakReference<>(loadingText);
-        new getXML(weakView, weakTextView).execute("https://rss.sciencedaily.com/matter_energy/chemistry.xml");
+        new getXML(weakView, weakTextView).execute("https://rss.sciencedaily.com/matter_energy/chemistry.xml",fullPath);
         mSwipeRefreshLayout = root.findViewById(R.id.swiperefresh);
         mSwipeRefreshLayout.setOnRefreshListener(this);
         return root;
@@ -85,7 +110,7 @@ public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         WeakReference<View> weakView;
         weakView = new WeakReference<>(mSwipeRefreshLayout.getRootView());
         WeakReference<TextView> weakTextView = new WeakReference<>(loadingText);
-        new getXML(weakView, weakTextView).execute("https://rss.sciencedaily.com/matter_energy/chemistry.xml");
+        new getXML(weakView, weakTextView).execute("https://rss.sciencedaily.com/matter_energy/chemistry.xml",fullPath);
         new Handler().postDelayed(new Runnable() {
             @Override public void run() {
                 mSwipeRefreshLayout.setRefreshing(false);
@@ -101,7 +126,6 @@ public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     private static final String TITLE = "title";
     private static final String ITEM = "item";
 
-
     //parse it
     static private class getXML extends AsyncTask<String, String, List<Event>> {
         WeakReference<View> view;
@@ -110,15 +134,23 @@ public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefre
             this.view=view;
             this.loadingViewNotif=textView;
         }
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
         protected List<Event> doInBackground(String... strings) {
             String rssFeed = strings[0];
+            File localFeed = null;
+            try {
+                localFeed = download_XML(rssFeed, strings[1]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             List<Event> list = new ArrayList<>();
             XmlPullParser parser = Xml.newPullParser();
             InputStream stream = null;
             try {
                 try {
-                    stream = new URL(rssFeed).openConnection().getInputStream();
+                    assert localFeed != null;
+                    stream = new FileInputStream(localFeed);
                     parser.setInput(stream, null);
                     int eventType = parser.getEventType();
                     boolean done = false;
@@ -187,6 +219,69 @@ public class NewsFragment extends Fragment implements SwipeRefreshLayout.OnRefre
                 loadingViewNotif.get().setVisibility(View.VISIBLE);
             }
             ((TextView)view.get().findViewById(R.id.itemsLoaded)).setText(eventsLoaded);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        private File download_XML(String urlString, String pathToSave) throws IOException {
+            File file = new File(pathToSave);
+            if(!file.exists()) {
+                URL url = new URL(urlString);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+
+                Log.v("Start Query", "Stream");
+                conn.connect();
+                Log.v("End Query", "Stream");
+                //read the result from the server
+                BufferedReader rdr = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sbr = new StringBuilder();
+                String line;
+                while ((line = rdr.readLine()) != null) {
+                    sbr.append(line).append('\n');
+                }
+
+                Log.v(sbr.toString(), "Stream");
+                try {
+                    try (FileOutputStream stream = new FileOutputStream(file)) {
+                        stream.write(sbr.toString().getBytes());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return file;
+        }
+    }
+    private void deleteFiles(String folder)
+    {
+        File dir = new File(folder);
+        if (!dir.exists())
+            return;
+        File[] files = dir.listFiles(new GenericExtFilter(".xml"));
+        assert files != null;
+        for (File file : files)
+        {
+            if (!file.isDirectory())
+            {
+                boolean result = file.delete();
+                Log.d("TAG", "Deleted:" + result);
+            }
+        }
+    }
+
+    class GenericExtFilter implements FilenameFilter {
+
+        private String ext;
+
+        private GenericExtFilter(String ext) {
+            this.ext = ext;
+        }
+
+        public boolean accept(File dir, String name) {
+            return (name.endsWith(ext));
         }
     }
 }
